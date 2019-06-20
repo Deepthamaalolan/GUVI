327d35136f4eade4190dd7463a93d5954a2a0e24
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2019-06-20 13:12:11 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,9 @@
 n=input()
-if(n=="a" or n=="e" or n=="i" or n=="o"or n=="u"or n=="A"or n=="E"or n=="I" or n=="O"or n=="U"):
+n.lower()
+if(n=="a" or n=="e" or n=="i" or n=="o"or n=="u"):
 	print("vowel")
-else if(n=="
+elif (n.isalpha()==0):
+	
+	print("invalid")
+else :
 	print("consonant")
