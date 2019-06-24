ed39e931b8295943af0b060900e3ef623223621d
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2019-06-24 08:58:20 +0530

    Create roman.py

---
+++ player/set1/roman.py
@@ -0,0 +1,12 @@
+def roman(n):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(n)):
+            if i > 0 and rom_val[n[i]] > rom_val[n[i - 1]]:
+                int_val += rom_val[n[i]] - 2 * rom_val[n[i - 1]]
+            else:
+                int_val += rom_val[n[i]]
+        return int_val
+
+n=input()
+print(roman(n))
